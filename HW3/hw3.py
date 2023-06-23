fbf02bdb8b48c655cf354ccff9bd5e2b6cad631f
Scherbakov Anatoly <axidarr@gmail.com>
2023-06-24 00:01:48 +0300

    Added HW3 part of code

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,5 +1,23 @@
 """
-Дан список повторяющихся элементов. Вернуть список
-с дублирующимися элементами. В результирующем списке
-не должно быть дубликатов
+Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами.
+В результирующем списке не должно быть дубликатов
 """
+
+elements_list = [5, 5, 4, 9, 7, 2, 7, 5, 4, 3, 1]
+doubled_elements_list = []
+
+print(f'Исходный список: {elements_list}')
+
+for index in range(len(elements_list)):
+    if elements_list.count(elements_list[index]) > 1 and elements_list[index] not in doubled_elements_list:
+        doubled_elements_list.append(elements_list[index])
+
+for el in elements_list:
+    if el in doubled_elements_list:
+        while elements_list.count(el) != 1:
+            elements_list.remove(el)
+
+#  elements_list = list(set(elements_list))
+
+print(f'Исходный список: {elements_list}')
+print(f'Исходный список: {doubled_elements_list}')
