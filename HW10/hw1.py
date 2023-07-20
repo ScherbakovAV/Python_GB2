e327ea44ce9d43283327d0dc0db65aa115642047
ScherbakovAV <axidarr@gmail.com>
2023-07-20 16:43:02 +0400

    Added ex1 from HW10

--- HW10/hw1.py
+++ HW10/hw1.py
@@ -9,6 +9,26 @@ from ex6 import Mammal
 
 
 class AnimalFabric():
-    def __init__(self, animal_class: class, *args, **kwargs):
+    def __init__(self, animal_type: str, name: str, uniq: int):
+        self.animal_type = animal_type
+        self.name = name
+        self.unique_param = uniq
+
+
+    def unique(self):
+        pass
+    
         
-        
+    def creating_animal(self):
+        if self.animal_type == 'Bird':
+            return Bird(self.name, self.unique_param)
+        elif self.animal_type == 'Fish':
+            return Fish(self.name, self.unique_param)
+        elif self.animal_type == 'Mammal':
+            return Mammal(self.name, self.unique_param)
+
+
+if __name__ == '__main__':
+    animal1 = AnimalFabric('Bird', 'Ласточка', 20)
+    bird1 = animal1.creating_animal()
+    print(bird1.unique())
