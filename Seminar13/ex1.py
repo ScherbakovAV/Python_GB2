fe0c25131ed391c7fc05ac2514a8a580b3008983
Scherbakov Anatoly <axidarr@gmail.com>
2023-07-27 21:52:52 +0300

    Added Seminar13

---
+++ Seminar13/ex1.py
@@ -0,0 +1,25 @@
+"""Создайте функцию, которая запрашивает числовые данные от
+пользователя до тех пор, пока он не введёт целое или
+вещественное число.
+Обрабатывайте не числовые данные как исключения."""
+
+
+def inp_numbers() -> float | int:
+    while True:
+        try:
+            num = input('Введите число... ')
+            num = int(num)
+            print('Это целое число')
+            break
+        except ValueError:
+            try:
+                num = float(num)
+                print('Это вещественное число')
+                break
+            except ValueError:
+                print(f'Значение должно быть целым или вещественным числом!')
+    return num
+
+
+if __name__ == '__main__':
+    inp_numbers()
